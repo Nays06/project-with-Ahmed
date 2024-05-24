cc454ef0df3e8395dc461be50eda64e92a13d67d
Ahmed <amartazanov892@gmail.com>
2024-05-25 00:08:41 +0300

    Commit 5

--- Main/scripts/main.js
+++ Main/scripts/main.js
@@ -1,32 +1,35 @@
 // create product cards script//
 const productCards = [
   {
+    id: 0,
     top: true,
     new: false,
     hit: false,
-    imgSrc: "./images/topProductImg1.png",
+    imgSrc: "./images/productImg1.png",
     priceWithDiscount: "3400 Р",
     discount: "-15%",
     priceWithoutDiscount: "4000 Р",
-    productName: "XBOX GAME PASS ULTIMATE + EA PLAY 14 JOURS | RENOUVELL",
+    productName: "Shape of the world",
     categories: ["Буст", "Origin"],
   },
   {
+    id: 1,
     top: true,
     new: false,
     hit: false,
-    imgSrc: "./images/topProductImg2.png",
+    imgSrc: "./images/productImg2.png",
     priceWithDiscount: "3400 Р",
     discount: "-15%",
     priceWithoutDiscount: "4000 Р",
-    productName: "Minecraft",
+    productName: "Journey",
     categories: ["Ключ", "Аккаунт Steam"],
   },
   {
+    id: 2,
     top: true,
     new: false,
     hit: false,
-    imgSrc: "./images/topProductImg3.png",
+    imgSrc: "./images/productImg3.png",
     priceWithDiscount: "3400 Р",
     discount: "-15%",
     priceWithoutDiscount: "4000 Р",
@@ -34,10 +37,155 @@ const productCards = [
     categories: ["Аккаунт", "Аккаунт Steam"],
   },
   {
+    id: 3,
     top: true,
     new: false,
     hit: false,
-    imgSrc: "./images/topProductImg4.png",
+    imgSrc: "./images/productImg4.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Shord Hike",
+    categories: ["Ключ", "EpicGames"],
+  },
+  {
+    id: 4,
+    top: false,
+    new: true,
+    hit: false,
+    imgSrc: "./images/productImg5.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Flower",
+    categories: ["Буст", "Origin"],
+  },
+  {
+    id: 5,
+    top: false,
+    new: true,
+    hit: true,
+    imgSrc: "./images/productImg6.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Hohokum",
+    categories: ["Ключ", "Аккаунт Steam"],
+  },
+  {
+    id: 6,
+    top: false,
+    new: true,
+    hit: false,
+    imgSrc: "./images/productImg7.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "THE WITNESS",
+    categories: ["Ключ", "Аккаунт Steam"],
+  },
+  {
+    id: 7,
+    top: false,
+    new: true,
+    hit: true,
+    imgSrc: "./images/productImg8.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Eastshade",
+    categories: ["Ключ", "EpicGames"],
+  },
+  {
+    id: 8,
+    top: false,
+    new: true,
+    hit: false,
+    imgSrc: "./images/productImg1.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Shape of the world",
+    categories: ["Буст", "Origin"],
+  },
+  {
+    id: 9,
+    top: false,
+    new: false,
+    hit: true,
+    imgSrc: "./images/productImg2.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Journey",
+    categories: ["Ключ", "Аккаунт Steam"],
+  },
+  {
+    id: 10,
+    top: false,
+    new: false,
+    hit: true,
+    imgSrc: "./images/productImg4.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Short Hike",
+    categories: ["Аккаунт", "Аккаунт Steam"],
+  },
+  {
+    id: 11,
+    top: false,
+    new: false,
+    hit: false,
+    imgSrc: "./images/productImg9.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Little Big Planet",
+    categories: ["Ключ", "EpicGames"],
+  },
+  {
+    id: 12,
+    top: false,
+    new: false,
+    hit: false,
+    imgSrc: "./images/productImg10.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "UNRAVEL",
+    categories: ["Буст", "Origin"],
+  },
+  {
+    id: 13,
+    top: false,
+    new: false,
+    hit: false,
+    imgSrc: "./images/productImg11.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Seasons after Fall",
+    categories: ["Ключ", "Аккаунт Steam"],
+  },
+  {
+    id: 14,
+    top: false,
+    new: false,
+    hit: false,
+    imgSrc: "./images/productImg12.png",
+    priceWithDiscount: "3400 Р",
+    discount: "-15%",
+    priceWithoutDiscount: "4000 Р",
+    productName: "Yonder: The Cloud Catcher Chronicle",
+    categories: ["Аккаунт", "Аккаунт Steam"],
+  },
+  {
+    id: 15,
+    top: false,
+    new: false,
+    hit: false,
+    imgSrc: "./images/productImg3.png",
     priceWithDiscount: "3400 Р",
     discount: "-15%",
     priceWithoutDiscount: "4000 Р",
@@ -52,7 +200,11 @@ function createProductCards() {
     productCard.className = "productCard";
     productCard.id = `productCard${i}`;
     productCard.innerHTML = `<img src="${productCards[i].imgSrc}" class="productImg" id="productImg${i}"/>
-                                    <div id="topBlock${i}" class="topBlock">Top <img src="./images/lightning.svg" alt=""> 4</div>
+                                    <div class="cardCategoriesBlock">
+                                      <div class="hitBlock" id="hitBlock${i}">Хит продаж</div>
+                                      <div class="newBlock" id="newBlock${i}">Новинка</div>
+                                      <div class="topBlock" id="topBlock${i}">Top <img src="./images/lightning.svg" alt=""> 4</div>
+                                    </div>
                                     <button id="addCardBtn${i}" class="addCardBtn">В корзину</button>
                                     <button id="addFavouritesBtn${i}" class="addFavouritesBtn"><img src="./images/likeImg.svg"></button>
                                     <div class="productCard_podBlock1">
@@ -73,14 +225,22 @@ function createProductCards() {
                                             <span>${productCards[i].categories[1]}</span>
                                         </div>
                                     </div>`;
-    document
-      .querySelector(".smallProductCardsConteiner")
-      .appendChild(productCard);
+    if(i < 4) document.querySelector(".smallProductCardsConteiner1").appendChild(productCard);
+    else document.querySelector(".smallProductCardsConteiner2").appendChild(productCard);
 
+    if (productCards[i].hit) {
+      const hitBlock = document.getElementById(`hitBlock${i}`);
+      hitBlock.style.display = "flex";
+    }
+    if (productCards[i].new) {
+      const newBlock = document.getElementById(`newBlock${i}`);
+      newBlock.style.display = "flex";
+    }
     if (productCards[i].top) {
       const topBlock = document.getElementById(`topBlock${i}`);
       topBlock.style.display = "flex";
     }
+    
   }
 }
 
