4fa016cf5c6ebd3841b3d40637fef67a203895fc
Nays06 <adam.ovsh00@mail.ru>
2024-05-25 11:53:58 +0300

    korzina i t.d.

--- страница товара/index.js
+++ страница товара/index.js
@@ -377,6 +377,15 @@ products.forEach((el) => {
     }
 })
 
+function randomArr(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+ads = randomArr(ads)
 
 function createProductCards() {
     for (let i = 0; i < ads.length; i++) {
